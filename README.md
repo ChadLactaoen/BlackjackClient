0d8f8344d59aebba940419bb8106594f46fbb1b2
Chad Tomas <chadlactaoen@gmail.com>
2015-11-09 15:56:18 -0800

    Updating README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # BlackjackClient
-Skeleton BlackjackClient to connect to Blackjack WebSocket server
+Skeleton BlackjackClient to connect to the Blackjack WebSocket server foune [here](https://github.com/chadtomas/Blackjack)
--- src/main/java/com/lactaoen/blackjack/client/BlackjackSessionHandler.java
+++ src/main/java/com/lactaoen/blackjack/client/BlackjackSessionHandler.java
@@ -7,13 +7,15 @@ import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 
+import java.util.UUID;
+
 /**
  * @author Chad Tomas
  */
 public class BlackjackSessionHandler extends StompSessionHandlerAdapter {
 
     // TODO Change to your own name. This is the name you will use to register with the server
-    private static final String NAME = "Nyan Cat"; //UUID.randomUUID().toString().substring(0, 6);
+    private static final String NAME = UUID.randomUUID().toString().substring(0, 6);
 
     @Override
     public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
